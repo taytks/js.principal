24a46ff22f453c690663cfeb95b2e0b613fb34eb
taytks <163296571+taytks@users.noreply.github.com>
2024-09-25 09:36:17 -0300

    mc

--- js/principal .js
+++ js/principal .js
@@ -1,2 +1,13 @@
 var titulo= document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes= document.querySelectorAll(".pacientes");
+for (var i = 0; i  <pacientes.length; i++) {
+     
+    var pacientes= pacientes [i];
+    var  tdpeso= pacientes.querySelector(".infor-peso");
+    var peso = tdpeso.textContent;
+    var tdaltura= paciente.querySelector(".info-altura");
+    var altura = tdaltura.textContent
+    var tdlm = pacientes.querySelector(".infor-imc");
+}
